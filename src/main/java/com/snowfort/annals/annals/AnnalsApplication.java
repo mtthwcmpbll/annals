b3167493f7086a115707be61aaebade45154c7e7
Matthew Campbell <mjc4648@gmail.com>
2018-01-04 10:50:28 -0500

    initial reactive spring boot project from generator

---
+++ src/main/java/com/snowfort/annals/annals/AnnalsApplication.java
@@ -0,0 +1,12 @@
+package com.snowfort.annals.annals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnalsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/snowfort/annals/annals/AnnalsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.snowfort.annals.annals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnnalsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
